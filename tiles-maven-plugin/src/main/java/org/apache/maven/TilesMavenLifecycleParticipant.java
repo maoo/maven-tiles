12f8af54d95bafc02be543155fc3aa11e4ae7b17
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2012-08-06 09:55:17 +0200

    renamed maven-tiles-core in tiles-maven-plugin (wicket example updated). README updated. Works like a charm.

---
+++ tiles-maven-plugin/src/main/java/org/apache/maven/TilesMavenLifecycleParticipant.java
@@ -0,0 +1,123 @@
+package org.apache.maven;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.model.merge.ModelMerger;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.sonatype.aether.RepositorySystem;
+import org.sonatype.aether.RepositorySystemSession;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.resolution.ArtifactRequest;
+import org.sonatype.aether.resolution.ArtifactResolutionException;
+import org.sonatype.aether.resolution.ArtifactResult;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.StringTokenizer;
+
+/**
+ * Fetches all dependencies defined in the POM <properties> as follows:
+ *   <properties>
+ *    <tile.1>it.session.maven.tile:maven-compile-tile:1.0-SNAPSHOT</tile.1>
+ *    <tile.2>it.session.maven.tile:maven-eclipse-tile:1.0-SNAPSHOT</tile.2>
+ *    <tile.3>it.session.maven.tile:maven-jetty-tile:1.0-SNAPSHOT</tile.3>
+ *   </properties>
+ *
+ * Dependencies are fetched using Aether {@link RepositorySystem}
+ * Merging operation is delegated to {@link ModelMerger}
+ */
+@Component(role = AbstractMavenLifecycleParticipant.class, hint = "TilesMavenLifecycleParticipant")
+public class TilesMavenLifecycleParticipant extends AbstractMavenLifecycleParticipant {
+
+  private MavenXpp3Reader reader = new MavenXpp3Reader();
+  private ModelMerger modelMerger = new ModelMerger();
+
+  @Requirement
+  private Logger logger;
+
+  @Requirement
+  private RepositorySystem repositorySystem;
+
+  protected File resolveArtifact(MavenProject currentProject,
+                                 String groupId,
+                                 String artifactId,
+                                 String version,
+                                 RepositorySystemSession repoSession) throws MojoExecutionException {
+    try {
+      Artifact tileArtifact = new DefaultArtifact(groupId, artifactId, "pom", version);
+      ArtifactRequest request = new ArtifactRequest();
+      request.setArtifact(tileArtifact);
+      request.setRepositories(currentProject.getRemoteProjectRepositories());
+      ArtifactResult result = this.repositorySystem.resolveArtifact(repoSession, request);
+      return result.getArtifact().getFile();
+    } catch (ArtifactResolutionException e) {
+      throw new MojoExecutionException(String.format("Error resolving artifact %s:%s:%s", groupId, artifactId, version));
+    }
+  }
+
+  /**
+   * Invoked after all MavenProject instances have been created.
+   * <p/>
+   * This callback is intended to allow extensions to manipulate MavenProjects
+   * before they are sorted and actual build execution starts.
+   */
+  public void afterProjectsRead(MavenSession session)
+      throws MavenExecutionException {
+    if (session.getCurrentProject().getBuild() != null) {
+
+      Enumeration propertyNames = session.getCurrentProject().getProperties().propertyNames();
+      while (propertyNames.hasMoreElements()) {
+        String propertyName = (String) propertyNames.nextElement();
+        if (propertyName.startsWith("tile.")) {
+          String propertyValue = session.getCurrentProject().getProperties().getProperty(propertyName);
+          StringTokenizer propertyTokens = new StringTokenizer(propertyValue, ":");
+
+          String groupId = propertyTokens.nextToken();
+          String artifactId = propertyTokens.nextToken();
+          String version = propertyTokens.nextToken();
+
+          String currentTileInformation =
+              String.format("'%s:%s:%s'",
+                  groupId,
+                  artifactId,
+                  version);
+
+          try {
+
+            File artifactFile = this.resolveArtifact(
+                session.getCurrentProject(),
+                groupId,
+                artifactId,
+                version,
+                session.getRepositorySession());
+
+            //TODO - need to improve here
+            Model tileModel = this.reader.read(new FileInputStream(artifactFile));
+            this.modelMerger.merge(session.getCurrentProject().getOriginalModel(), tileModel, false, null);
+            this.modelMerger.merge(session.getCurrentProject().getModel(), tileModel, false, null);
+            this.modelMerger.merge(session.getTopLevelProject().getModel(), tileModel, false, null);
+            logger.info(String.format("Loaded Maven Tile " +currentTileInformation));
+          } catch (FileNotFoundException e) {
+            throw new MavenExecutionException("Error loading tile " + currentTileInformation, e);
+          } catch (XmlPullParserException e) {
+            throw new MavenExecutionException("Error building tile " + currentTileInformation, e);
+          } catch (IOException e) {
+            throw new MavenExecutionException("Error parsing tile " + currentTileInformation, e);
+          } catch (MojoExecutionException e) {
+            throw new MavenExecutionException("Error retrieving tile " + currentTileInformation, e);
+          }
+        }
+      }
+    }
+  }
+}
