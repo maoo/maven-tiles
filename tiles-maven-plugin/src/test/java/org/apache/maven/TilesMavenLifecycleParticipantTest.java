cd4d615c312652e235387cd9af2f620425a61dbb
tony.franca <tony.franca@touchtec.com.br>
2012-09-05 14:54:14 -0300

    Fixing test "testResolveArtifact"

--- tiles-maven-plugin/src/test/java/org/apache/maven/TilesMavenLifecycleParticipantTest.java
+++ tiles-maven-plugin/src/test/java/org/apache/maven/TilesMavenLifecycleParticipantTest.java
@@ -1,6 +1,14 @@
 package org.apache.maven;
 
-import org.apache.maven.execution.MavenSession;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.junit.Test;
@@ -13,11 +21,6 @@ import org.sonatype.aether.resolution.ArtifactRequest;
 import org.sonatype.aether.resolution.ArtifactResolutionException;
 import org.sonatype.aether.resolution.ArtifactResult;
 import org.sonatype.aether.util.artifact.DefaultArtifact;
-
-import java.io.File;
-
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TilesMavenLifecycleParticipantTest {
@@ -45,19 +48,26 @@ public class TilesMavenLifecycleParticipantTest {
         assertNotNull(request);
     }
 
-    //This test is not working yet; cannot mock mockRepositorySystem.resolveArtifact
+    //This test is not working yet; cannot mtiles-maven-plugin/src/test/java/org/apache/maven/TilesMavenLifecycleParticipantTest.javaock mockRepositorySystem.resolveArtifact
     @Test
     public void testResolveArtifact() throws ArtifactResolutionException, MojoExecutionException {
         RepositorySystem mockRepositorySystem = mock(RepositorySystem.class);
         RepositorySystemSession mockRepositorySystemSession = mock(RepositorySystemSession.class);
-        Artifact artifact = participant.getArtifactFromCoordinates(groupId,artifactId,version);
-        MavenProject mockMavenProject = mock(MavenProject.class);
-        ArtifactRequest mockRequest = participant.getArtifactRequestFromArtifact(artifact, mockMavenProject);
-        ArtifactResult mockArtifactResult = mock(ArtifactResult.class);
+        MavenProject emptyMavenProject = new MavenProject();
+        ArtifactResult dummyArtifactResult = makeDummyArtifactResult();
+        when(mockRepositorySystem.resolveArtifact(same(mockRepositorySystemSession), any(ArtifactRequest.class))).thenReturn(dummyArtifactResult);
+
         participant.setRepositorySystem(mockRepositorySystem);
-        when(mockRepositorySystem.resolveArtifact(mockRepositorySystemSession, mockRequest)).thenReturn(mockArtifactResult);
-        File artifactFile = participant.resolveArtifact(mockMavenProject, groupId, artifactId, version, mockRepositorySystemSession);
+        File artifactFile = participant.resolveArtifact(emptyMavenProject, groupId, artifactId, version, mockRepositorySystemSession);
+
         assertNotNull(artifactFile);
     }
+
+	private ArtifactResult makeDummyArtifactResult() {
+		ArtifactResult dummyArtifactResult = new ArtifactResult(new ArtifactRequest());
+		DefaultArtifact dummyArtifact = new DefaultArtifact("dummy:dummy:1");
+        dummyArtifactResult.setArtifact(dummyArtifact.setFile(new File("i_dont_exist"))); //This setFile method is not what it looks like o.O 
+        return dummyArtifactResult;
+	}
 
 }
